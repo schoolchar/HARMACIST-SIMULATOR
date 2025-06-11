861582d9af63b8d4ebd7cffaf3ef7b15e03be371
schoolchar <br295143@ucf.edu>
2025-06-11 15:41:30 -0400

    hi

---
+++ Assets/Scripts/Main Scripts/DialogueContainer.cs
@@ -0,0 +1,12 @@
+
+using UnityEngine;
+using TMPro;
+
+[System.Serializable]
+
+public class DialogueContainer
+{
+    public GameObject root;
+    public TMP_Text nameText;
+    public TMP_Text dialogueText;    
+}
